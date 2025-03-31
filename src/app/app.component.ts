52459f379336bb4606a3336ce2676c39afcce787
mostov4 <mostajab12@hotmail.com>
2025-03-31 22:22:54 +0500

    components creation (ng g c <component name>) and standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { SingleFileComponentComponent } from "./auth/singleFileComponent/single-file-component/single-file-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, SignupComponent, SingleFileComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/auth/singleFileComponent/single-file-component/single-file-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-file-component',
+  imports: [],
+  template: "<h5 class='text'>Single File Component</h5>",
+  styles: ['.text{color: purple}']
+})
+export class SingleFileComponentComponent {
+
+}
